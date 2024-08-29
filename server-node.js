f215895982472acc1705248382bf768fe820b363
Your Name <your.email@example.com>
2024-08-29 13:56:29 +0530

    basic server setup

--- server-node.js
+++ server-node.js
@@ -1,1 +1,15 @@
-console.log("nodejs with bun.js");
+const http = require("http");
+
+const hostname = "127.0.0.1";
+
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("hello satyam, learn bun.js");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`server is listening at http://${hostname}:${port}`);
+});
